431df2a89f8160dade19b053c2561095fbd5e3d3
Mark Stephen Gonzales <cosmoslayer@gmail.com>
2021-09-13 16:19:54 +0800

    Finish Stage 4/8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -3,8 +3,9 @@ import random
 
 word_list = ['python', 'java', 'kotlin', 'javascript']
 random_word = random.choice(word_list)
+hint_word = random_word[0:3] + "-" * (len(random_word) - 3)
 print("H A N G M A N")
-answer = input("Guess the word:")
+answer = input(f"Guess the word: {hint_word}")
 if answer == random_word:
     print("You survived!")
 else:
