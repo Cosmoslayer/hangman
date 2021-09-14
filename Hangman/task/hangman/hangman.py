c4bc809fb409626662a962e6b4d183dae4c5ee68
Mark Stephen Gonzales <cosmoslayer@gmail.com>
2021-09-14 17:09:00 +0800

    Finish Stage 6/8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -5,19 +5,28 @@ word_list = ['python', 'java', 'kotlin', 'javascript']
 random_word = random.choice(word_list)
 hint_word = "-" * (len(random_word))
 letters_list = list(random_word)
-letters_set = set(random_word)
+letters_set = set()
 tries = 8
 print("H A N G M A N")
 while tries > 0:
-    print(f"\n{hint_word}\nInput a letter: ")
-    answer = input()
+    print(f"\n{hint_word}")
+    answer = input("Input a letter: ")
     if answer not in random_word:
         print("That letter doesn't appear in the word")
+        tries -= 1
     else:
         if answer in letters_set:
+            print("No improvements")
+            tries -= 1
+        else:
+            letters_set.add(answer)
             for i in range(len(letters_list)):
                 if letters_list[i] == answer:
                     position = i
                     hint_word = hint_word[:position] + answer + hint_word[position + 1:]
-    tries -= 1
-print("Thanks for playing!\nWe'll see how well you did in the next stage")
+            if "-" not in hint_word:
+                print(f"\n{random_word}")
+                print("You guessed the word!\nYou survived!")
+                break
+else:
+    print("You lost!")
